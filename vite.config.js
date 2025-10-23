e735306cb177796b2b14a74e9bb149f82294bedf
savita-aher <savigahire@gmail.com>
2025-10-23 14:38:52 -0400

    switch to static.json for render routing

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-
-export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.' // copies into dist/
-        }
-      ]
-    })
-  ]
-});
-
-
-/*import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -31,4 +12,4 @@ export default defineConfig({
       }
     }
   }
-});*/
+});
