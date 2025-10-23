930d264d086014533a3509b6d7e8490a37aab3b0
savita-aher <savigahire@gmail.com>
2025-10-23 11:47:23 -0400

    fixed path

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
-import '../styles/faqSection.css';
+import '../styles/FaqSection.css';
 
 const FAQ = () => {
   const [faqs, setFaqs] = useState([]);
